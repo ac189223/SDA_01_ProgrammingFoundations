55a38cff2f97dab0c2f8d77bc3f7a354e451ac12
andrzejcalka <ac189223@o2.pl>
2019-10-14 12:11:07 +0200

    LinkedListCreation InitialCommit

---
+++ src/PID01/Ex01/LinkedList.java
@@ -0,0 +1,27 @@
+package PID01.Ex01;
+
+class Node {                                                // Elements of our list
+    int elem;                                               // Identification
+    Node next;                                              // Pointing to next element
+
+    public Node(int elem, Node next) {
+        this.elem = elem;
+        this.next = next;
+    }
+}
+
+public class LinkedList {
+    Node first = null;
+
+    public void add(int elem) {
+        if (first == null)                                  // LinkedList is empty
+            first = new Node(elem, null);             // Create first element
+        else {
+            Node current = first;                           // Starting from first
+            while (current.next != null)                    // If there exist next one
+                current = current.next;                     // Then go to successor
+            current.next = new Node(elem, null);      // Add new one after "no successor" element
+        }
+
+    }
+}
